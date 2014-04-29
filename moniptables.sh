9f6fd7f1979aaed0a61683ba6075e4e2d99132de
Sylàn <sylantutos@users.noreply.github.com>
2014-04-30 00:28:31 +0200

    Update moniptables.sh

--- moniptables.sh
+++ moniptables.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
 ### BEGIN INIT INFO
-# Provides: R�gles iptables
+# Provides: Règles iptables
 # Required-Start: $remote_fs $syslog
 # Required-Stop: $remote_fs $syslog
 # Default-Start: 2 3 4 5
 # Default-Stop: 0 1 6
-# Short-Description: R�gles iptables lestutosdesylan.com
+# Short-Description: Règles iptables lestutosdesylan.com
 ### END INIT INFO
 
-# Mise � 0 des r�gles
+# Mise à 0 des règles
 iptables -t filter -F
 iptables -t filter -X
-echo "Mise � 0 des r�gles - Check"
+echo "Mise à 0 des règles - Check"
 
-# On bloque tout par d�faut
+# On bloque tout par défaut
 iptables -t filter -P INPUT DROP
 iptables -t filter -P FORWARD DROP
 iptables -t filter -P OUTPUT DROP
-echo "Tout bloqu� par d�faut - Check"
+echo "Tout bloqué par défaut - Check"
 
-# On garde les connexions d�j� �tablies
+# On garde les connexions déjà établies
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 
@@ -29,12 +29,12 @@ iptables -t filter -A INPUT -i lo -j ACCEPT
 iptables -t filter -A OUTPUT -o lo -j ACCEPT
 echo "Localhost - Check"
 
-# On autorise la r�ponse aux ping (ICMP)
+# On autorise la réponse aux ping (ICMP)
 iptables -t filter -A INPUT -p icmp -j ACCEPT
 iptables -t filter -A OUTPUT -p icmp -j ACCEPT
 echo "Ping - Check"
 
-# On autorise les acc�s SSH (entrant/sortant)
+# On autorise les accès SSH (entrant/sortant)
 iptables -t filter -A INPUT -p tcp --dport 1337 -j ACCEPT
 iptables -t filter -A INPUT -p udp --dport 1337 -j ACCEPT
 iptables -t filter -A OUTPUT -p tcp --dport 1337 -j ACCEPT
@@ -62,7 +62,7 @@ iptables -t filter -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -t filter -A INPUT -p tcp --dport 8443 -j ACCEPT
 echo "http(s) - Check"
 
-# On autorise les acc�s FTP
+# On autorise les accès FTP
 iptables -t filter -A OUTPUT -p tcp --dport 2121 -j ACCEPT
 iptables -t filter -A OUTPUT -p tcp --dport 20 -j ACCEPT
 iptables -t filter -A INPUT -p tcp --dport 20 -j ACCEPT
