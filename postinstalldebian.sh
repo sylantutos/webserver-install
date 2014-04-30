8196051811bf2d31b815fb93ca8c60726271bdbd
Sylàn <sylantutos@users.noreply.github.com>
2014-04-30 13:44:25 +0200

    Update postinstalldebian.sh
    Remplacement des accents
    Ajout de la désactivation de l'ipv6

--- postinstalldebian.sh
+++ postinstalldebian.sh
@@ -6,15 +6,20 @@ useradd -G root -s /bin/bash -d /home/sylan -m sylan
 passwd sylan
 adduser sylan adm
 
-#On va mettre à jour le serveur
-#Et on ajoute les sources du projet DotDeb pour avoir les derniers patchs de sécurité
+#On va mettre a jour le serveur
+#Et on ajoute les sources du projet DotDeb pour avoir les derniers patchs de securite
 echo "deb http://packages.dotdeb.org wheezy all" >> /etc/apt/sources.list
 echo "deb-src http://packages.dotdeb.org wheezy all" >> /etc/apt/sources.list
 wget http://www.dotdeb.org/dotdeb.gpg
 cat dotdeb.gpg | sudo apt-key add -
-#Et on met à jour
+#Et on met a jour
 apt-get update
 apt-get upgrade -y
+
+#On dÃ©sactive l'ipv6 (peu d'intÃ©rÃªt d'y passer)
+echo '1' > /proc/sys/net/ipv6/conf/lo/disable_ipv6   
+echo '1' > /proc/sys/net/ipv6/conf/all/disable_ipv6   
+echo '1' > /proc/sys/net/ipv6/conf/default/disable_ipv6
 
 #On installe bash-completion
 apt-get install -y bash-completion
@@ -36,18 +41,18 @@ echo "alias l='ls $LS_OPTIONS -lAh'" >> /etc/bash.bashrc
 
 #On installe sudo
 apt-get install -y sudo
-#Et on le configure pour autoriser les membres du groupe root à pouvoir utiliser sudo
+#Et on le configure pour autoriser les membres du groupe root a pouvoir utiliser sudo
 echo "%root ALL=(ALL) PASSWD: ALL" >> /etc/sudoers
-#Et petit bonus avec des étoiles lors d'une demande de mot de passe
+#Et petit bonus avec des etoiles lors d'une demande de mot de passe
 sed -i 's|env_reset|env_reset,pwfeedback|' /etc/sudoers
 
-#Sécurisons un peu ssh avec remplacement du port et interdiction de se connecter en root 
+#Securisons un peu ssh avec remplacement du port et interdiction de se connecter en root 
 #Il faudra utiliser notre nouvel utilisateur
 sed -i 's|Port 22|Port 1337|' /etc/ssh/sshd_config
 sed -i 's|PermitRootLogin yes|PermitRootLogin no|' /etc/ssh/sshd_config
 /etc/init.d/ssh restart
 
-#On sécurise le serveur avec iptables
+#On securise le serveur avec iptables
 cd /etc/init.d/
 wget --no-check-certificate https://raw.githubusercontent.com/sylantutos/webserver-install/master/moniptables.sh
 chmod +x moniptables.sh
@@ -65,9 +70,9 @@ motdstat --generate
 echo "nginx" >> /etc/motdstat/process
 echo "php5-fpm" >> /etc/motdstat/process
 echo "mysqld" >> /etc/motdstat/process
-#On mets à jour le Message Of The Day toutes les 5mn
+#On mets a jour le Message Of The Day toutes les 5mn
 (crontab -l 2>/dev/null; echo "*/5 * * * * /usr/bin/motdstat --generate") | crontab -
-#Et on configure tout ça
+#Et on configure tout ca
 echo "/                               80      90" >> /etc/motdstat/fstab_limits
 echo "/var                               80      90" >> /etc/motdstat/fstab_limits
 
@@ -80,3 +85,4 @@ echo "\___ \| | | | |/ _\` | '_ \  \___ \ / _ \ '__\ \ / / _ \ '__|" >> /etc/mot
 echo " ___) | |_| | | (_| | | | |  ___) |  __/ |   \ V /  __/ |   " >> /etc/motd.orig
 echo "|____/ \__, |_|\__,_|_| |_| |____/ \___|_|    \_/ \___|_|   " >> /etc/motd.orig
 echo "       |___/                                                " >> /etc/motd.orig
+
